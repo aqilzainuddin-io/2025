da03dced69a47544da09276dcc8f960f9d33f09f
aqilzainuddin-writer <aqilzainuddin.io@gmail.com>
2026-01-26 13:08:57 +0800

    test: add login.spec.ts file

---
+++ tests/login.spec.ts
@@ -0,0 +1,42 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+
+test.describe('Login functionality', () => {
+
+  test('User can login with valid credentials', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.goto();
+    await loginPage.login('standard_user', 'secret_sauce');
+
+    await expect(page).toHaveURL(/inventory/);
+  });
+
+  test('User cannot login with invalid credentials', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.goto();
+    await loginPage.login('invalid_user', 'invalid_password');
+
+    await expect(loginPage.invalidCredMsg).toHaveText('Epic sadface: Username and password do not match any user in this service');
+  });
+
+  test('Username is required error message is displayed', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.goto();
+    await loginPage.login('', 'some_password');
+
+    await expect(loginPage.invalidCredMsg).toHaveText('Epic sadface: Username is required');
+  });
+
+  test('Password is required error message is displayed', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.goto();
+    await loginPage.login('some_user', '');
+
+    await expect(loginPage.invalidCredMsg).toHaveText('Epic sadface: Password is required');
+  });
+
+});
