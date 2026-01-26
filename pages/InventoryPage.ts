d9bbfd05c7a79b1282c9d34e8fa84123bc8a7625
aqilzainuddin-writer <aqilzainuddin.io@gmail.com>
2026-01-26 21:24:25 +0800

    feat: add InventoryPage.ts file

---
+++ pages/InventoryPage.ts
@@ -0,0 +1,34 @@
+import { Page, Locator } from '@playwright/test';
+
+export class InventoryPage {
+  readonly page: Page;
+
+  readonly inventoryItems: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+
+    this.inventoryItems = page.getByTestId('inventory-item');
+  }
+
+  getItemByName(itemName: string): Locator {
+    return this.inventoryItems.filter({ hasText: itemName });
+  }
+
+  getItemName(itemName: string): Locator {
+    return this.getItemByName(itemName).getByTestId('inventory-item-name');
+  }
+
+  getItemDescription(itemName: string): Locator {
+    return this.getItemByName(itemName).getByTestId('inventory-item-desc');
+  }
+
+  getItemPrice(itemName: string): Locator {
+    return this.getItemByName(itemName).getByTestId('inventory-item-price');
+  }
+
+  async addItemToCart(itemName: string): Promise<void> {
+    const item = this.getItemByName(itemName);
+    await item.getByRole('button', { name: 'Add to cart' }).click();
+  }
+}
