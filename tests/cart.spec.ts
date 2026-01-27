a8fa1c4e30a227300250ba60a40772378fc34cca
aqilzainuddin-writer <aqilzainuddin.io@gmail.com>
2026-01-27 09:56:59 +0800

    chore: update cart.spec.ts file with comments

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -11,6 +11,7 @@ test('User can proceed to checkout from cart', async ({ page }) => {
   await loginPage.goto();
   await loginPage.login('standard_user', 'secret_sauce');
 
+  // define products to be added to cart
   const products = [
     { name: 'Sauce Labs Backpack', price: '$29.99' },
     { name: 'Sauce Labs Bike Light', price: '$9.99' },
@@ -20,12 +21,15 @@ test('User can proceed to checkout from cart', async ({ page }) => {
     { name: 'Test.allTheThings() T-Shirt (Red)', price: '$15.99' },
   ];
 
+  // add all products to cart
   for (const product of products) {
     await inventoryPage.addItemToCart(product.name);
   }
 
+  // navigate to cart page
   await inventoryPage.goToCart();
 
+  // verify cart items details
   for (const product of products) {
     await expect(cartPage.getCartItemName(product.name)).toHaveText(product.name);
     await expect(cartPage.getCartItemDescription(product.name)).toBeVisible();
