6ba9e1720189e6e3e8daf0b087386e17b143947d
aqilzainuddin-writer <aqilzainuddin.io@gmail.com>
2026-01-27 11:56:40 +0800

    chore: remove example.spec.ts file

--- jsonplaceholder-automation/tests/example.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('basic test', async ({ page }) => {
-  await page.goto('https://jsonplaceholder.typicode.com/');
-  expect(await page.title()).toBe('JSONPlaceholder - Free Fake REST API');
-});
